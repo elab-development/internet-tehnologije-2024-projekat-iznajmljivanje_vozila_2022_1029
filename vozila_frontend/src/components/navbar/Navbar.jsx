07ff48cc64870d2a62f299c46798224f882e5f10
Nemanjaa <nemalic25@gmail.com>
2025-01-27 00:32:30 +0100

    Dodat logo u okviru navigacionog menija

--- vozila_frontend/src/components/navbar/Navbar.jsx
+++ vozila_frontend/src/components/navbar/Navbar.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; 
+import './Navbar.css';
+import logo from '../images/logo3.png'; 
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/" className="nav-link nav-brand">Iznajmljivanje vozila</Link>
+      <div className="nav-brand-container">
+        <img src={logo} alt="Logo" className="nav-logo" />
+        <Link to="/" className="nav-link nav-brand">Iznajmljivanje vozila</Link>
+      </div>
       <div className="nav-links">
-        <Link to="/" className="nav-link">Pocetna</Link>
+        <Link to="/" className="nav-link">Početna</Link>
         <Link to="/about" className="nav-link">O nama</Link>
-        <Link to="/cars" className="nav-link">Nasa vozila</Link>
-        
+        <Link to="/cars" className="nav-link">Naša vozila</Link>
       </div>
     </nav>
   );
 };
+
 export default Navbar;
